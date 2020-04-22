472997e10fa8ab01e2386325dc9eb00388f3f62b
Vinicius Cornieri <vinicius.cornieri@matera.com>
2020-04-22 00:21:50 -0300

    adding repositories post and put logic

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nível 01 - Desafio conceitos do NodeJS
+
+Neste desafio do Bootcamp GoStack 11 foi proposto criar uma aplicação para armazenar repositórios do nosso portfólio. Essa aplicação deve permitir fazer CRUD de repositórios e permitir que estes possam receber "likes" utilizando os conceitos de NodeJS aprendidos no bootcamp até então. 
+
+A aplicação deve passar nos testes recebidos recebidos no template.
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const repositoryInputValidate = require("./app/middlewares/repositoryInputValidate");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,15 +11,41 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", repositoryInputValidate, (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const newRepository = {
+    id: uuid(),
+    likes: 0,
+    title,
+    url,
+    techs,
+  };
+
+  repositories.push(newRepository);
+  return response.json(newRepository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", repositoryInputValidate, (request, response) => {
+  const { id } = request.params;
+  const repository = repositories.find(repo => repo.id === id);
+
+  if (!repository) {
+    return response
+      .status(400)
+      .json({ error: `Could not found repository ${id}` });
+  }
+
+  const { title, url, techs } = request.body;
+
+  repository.title = title || repository.title;
+  repository.url = url || repository.url;
+  repository.techs = techs || repository.techs;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
